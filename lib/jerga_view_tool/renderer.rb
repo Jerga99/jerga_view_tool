fb3ff428e9b242119c103ca3caf720e81ef948c0
Filip Jerga <filipjerga@Filips-MacBook-Pro.local>
2018-02-02 01:07:01 +0100

    Adds module

--- lib/jerga_view_tool/renderer.rb
+++ lib/jerga_view_tool/renderer.rb
@@ -1,5 +1,8 @@
- class Renderer
-  def self.copyright name, msg
+
+module JergaViewTool
+  class Renderer
+    def self.copyright name, msg
     "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
   end
 end
